0885cb66a0fc6771f9ecea84c2e0b74bae7bf057
Sophie Archibald <arc21004@byui.edu>
2025-09-27 12:28:43 -0600

    Added user input in the console for menu selection

--- FinanceTracker.java
+++ FinanceTracker.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class FinanceTracker {
+
+    String menu = "1. Add new transaction\n2. Get account balance\n3. View all past transactions\n4. Quit program";
+
     public static void main(String[] args) {
+
+        FinanceTracker tracker = new FinanceTracker();
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("This is the finance tracker");
+        tracker.displayMenu();
+        String userInput = scanner.nextLine();
+        System.out.println("User selected: " + userInput);
+    }
+
+    private void displayMenu() {
+        System.out.println("Menu:");
+        System.out.println(menu);
+        System.out.print("Please select one of the menu options from above: ");
+        
     }
     
 }
